76db2419ce4bf79ac104ce502b2c869ebfb7cda1
lair-venom <mir.devil50@gmail.com>
2025-07-21 01:53:08 +0300

    Update PaymentModal.tsx

--- src/components/ui/PaymentModal.tsx
+++ src/components/ui/PaymentModal.tsx
@@ -135,9 +135,8 @@ export const PaymentModal: React.FC<PaymentModalProps> = ({
               href="https://t.me/venom_vp_bot"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center justify-center w-full bg-orange-500 hover:bg-orange-600 text-white py-3 px-4 rounded-lg transition-colors"
+              className="flex items-center justify-center w-full bg-gray-600 hover:bg-gray-500 text-white py-3 px-4 rounded-lg transition-colors"
             >
-          <i class="fa fa-telegram" aria-hidden="true"></i>
 
               Telegram: @venom_vp_bot
             </a>
@@ -147,10 +146,6 @@ export const PaymentModal: React.FC<PaymentModalProps> = ({
               rel="noopener noreferrer"
               className="flex items-center justify-center w-full bg-gray-600 hover:bg-gray-500 text-white py-3 px-4 rounded-lg transition-colors"
             >
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-  <path d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z" />
-</svg>
-
 
               Discord сервер
             </a>
